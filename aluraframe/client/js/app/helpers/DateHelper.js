997e918768d3a9dcb77c5ad599df6c060b917cbb
Renan Schmitt <renan.schmitt@dbserver.com.br>
2020-02-20 17:53:46 -0300

    First commit now

---
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,18 @@
+class DateHelper {
+
+
+    constructor() {
+        throw new Error('DateHelper cant be instanced');
+    }
+    
+    static dataParaTexto(data){
+        return data.getDate()
+          + '/' + (data.getMonth() + 1)
+          + '/' + data.getFullYear();
+    }
+
+    static textoParaData(texto){
+        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+    }
+    
+}
